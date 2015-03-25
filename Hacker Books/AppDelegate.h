fe6a10440a6a4e648c51257b97ec03c38d2c8264
Alejandro Rodriguez <alejo80805@hotmail.com>
2015-03-24 22:06:08 -0500

    Initial Commit

---
+++ Hacker Books/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hacker Books
+//
+//  Created by Alejandro Rodriguez on 3/24/15.
+//  Copyright (c) 2015 Alejandro Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hacker BooksTests/Hacker_BooksTests.m
@@ -0,0 +1,40 @@
+//
+//  Hacker_BooksTests.m
+//  Hacker BooksTests
+//
+//  Created by Alejandro Rodriguez on 3/24/15.
+//  Copyright (c) 2015 Alejandro Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Hacker_BooksTests : XCTestCase
+
+@end
+
+@implementation Hacker_BooksTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
